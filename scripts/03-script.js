af97a5fc3cce8b4b961914cf7d3fe42e1b907ae3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-19 23:53:29 +0300

    Feat(JS) Added script

---
+++ scripts/03-script.js
@@ -0,0 +1,17 @@
+const displayCountryByGPS = (lat, lng) => {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Problem with geocoding (error ${response.status})`);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+    })
+    .catch(error => console.error(`${error.message} 🫠`));
+};
+
+displayCountryByGPS(35.756, 139.256);
+displayCountryByGPS(48.857, 2.358);
+displayCountryByGPS(40.708, -74.051);
