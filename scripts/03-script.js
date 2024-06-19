f3231d3d578ca08613db176e5814d4e9c89b4604
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2024-06-20 00:09:38 +0300

    Update 03-script.js

--- scripts/03-script.js
+++ scripts/03-script.js
@@ -1,3 +1,51 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const displayCountry = function (data, className = '') {
+  const currencies = data.currencies;
+  const currencyName = Object.values(currencies)[0].name;
+  const currencySymbol = Object.values(currencies)[0].symbol;
+
+  const languages = data.languages;
+  const firstLanguage = Object.values(languages)[0];
+
+  const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👨‍👩‍👧‍👦</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} millions</p>
+      <p class="country__row"><span>🗣️</span>${firstLanguage}</p>
+      <p class="country__row"><span>💰</span>${currencySymbol} ${currencyName}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const displayError = function (message) {
+  countriesContainer.insertAdjacentText('beforeend', message);
+  countriesContainer.style.opacity = 1;
+};
+
+const getDataAndConvertToJSON = function (
+  url,
+  errorMessage = 'Something went wrong.'
+) {
+  return fetch(url).then(response => {
+    if (!response.ok)
+      throw new Error(`${errorMessage} Error ${response.status}`);
+    return response.json();
+  });
+};
+
 const displayCountryByGPS = (lat, lng) => {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(response => {
@@ -8,7 +56,20 @@ const displayCountryByGPS = (lat, lng) => {
     .then(data => {
       console.log(data);
       console.log(`You are in ${data.city}, ${data.country}`);
+      return getDataAndConvertToJSON(
+        `https://restcountries.com/v3.1/name/${data.country.toLowerCase()}`,
+        'Country not found.'
+      );
     })
+    .then(data => displayCountry(data[0], 'neighbour'))
+    .catch(e => {
+      console.error(`${e} 🧐`);
+      displayError(`Something went wrong 🧐: ${e.message} Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    })
+
     .catch(error => console.error(`${error.message} 🫠`));
 };
 
